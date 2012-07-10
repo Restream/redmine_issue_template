2b79fc4e69d61f84bf4b619030a0170b777514b8
Vladimir <nettsundere@gmail.com>
2012-07-10 20:47:36 +0400

    Fixed bugs, fixed redmine v 1.4 compatibility, fixed ui

--- init.rb
+++ init.rb
@@ -7,11 +7,16 @@ Redmine::Plugin.register :redmine_issue_template do
   version '0.0.1'
   url 'http://undev.ru'
   author_url 'http://undev.ru'
-  menu :admin_menu, :issue_templates, { :controller => 'issue_templates', :action => 'index' }
-end
 
+  icon = "/plugin_assets/redmine_issue_template/images/issue_templates.gif"
+  plugin_menu_html = {
+    :style => "background-image: url(#{icon})"
+  }
+  menu(:admin_menu, :issue_templates, {
+    :controller => 'issue_templates',
+    :action => 'index'
+  }, :html => plugin_menu_html)
+end
 
 ApplicationHelper.send(:include, IssueTemplatesHelper)
 require 'redmine_issue_template/hooks/view_issues_form_details_bottom_hook'
-
-
